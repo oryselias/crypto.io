c8621897d47be497f6db8b80da47ec1caca2a02d
oryselias <76832317+oryselias@users.noreply.github.com>
2022-12-22 16:39:26 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -49,7 +49,7 @@ function Header() {
               variant="h6"
               className={classes.title}
             >
-              cryptogeek
+              Crypto.io
             </Typography>
             {/* <Button color="inherit">Login</Button> */}
             <Select
